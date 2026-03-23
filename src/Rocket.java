fb141ca702c5a945b52b222ca3047d8c39ba5bbc
vhdez <vhernandez@scienceleadership.org>
2026-03-23 13:56:35 -0400

    Add recursive algorithm for launch.

--- src/Rocket.java
+++ src/Rocket.java
@@ -10,6 +10,31 @@ public class Rocket implements Rocketish {
 
     // Method
     public boolean launch() {
+        launchRecursive();
+        return true;
+    }
+
+    public boolean launchRecursive() {
+        if (countdownTime == 0) {
+            return true;
+        }
+
+        System.out.println(rocketName + " will launch in " + countdownTime + " seconds.");
+        try {
+            Thread.sleep(1000); // Wait 1 second
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();   // restore flag
+            System.out.println("Sleep was interrupted");
+        }
+
+        countdownTime = countdownTime - 1;
+        launchRecursive();
+
+        return true;
+    }
+
+
+    public boolean launchIterative() {
         for (int i = countdownTime; i > 0; i = i  - 1) {
             System.out.println(rocketName + " will launch in " + i + " seconds.");
             try {
